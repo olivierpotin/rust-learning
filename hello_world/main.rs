b45a86798c68cab937b4c03392ae57895ed86119
Olivier Potin <olivier.potin83@gmail.com>
2017-06-10 10:24:23 +0200

    My first Rust source code

---
+++ hello_world/main.rs
@@ -0,0 +1,16 @@
+
+// Rust is an ahead-of-time compiled language
+
+
+// Anatomy of a Rust program
+// Definition of a function called main that has no parameters and returns nothing
+// function body is wrapped in curly braces
+
+
+fn main() {
+// Rust style is to indent with 4 spaces, not a tab
+// this is calling a Rust macro which is how metaprogramming is done in Rust
+// calling a function would be done without the !
+// most lines in Rust ends with ;
+    println!("Hello, world");
+}
